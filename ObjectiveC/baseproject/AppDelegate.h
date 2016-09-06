acccd7d02c288ee0bcd0fc72c40afbd51db1ecd4
Tomas Rodriguez <tomasrc@hellobaytree.com>
2016-09-06 17:13:52 -0300

    Merging both iOS base projects into 1 Project

---
+++ ObjectiveC/baseproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baseproject
+//
+//  Created by Joel Marquez on 1/29/16.
+//  Copyright © 2016 Baytree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
